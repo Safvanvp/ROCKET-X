9e02adb44558bcc5815ad94bdcd0f1c359187c8f
Safvan vp <167276814+Safvanvp@users.noreply.github.com>
2025-05-14 08:05:19 +0530

    light and dark mode

---
+++ lib/theme/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Color(0xff212121),
+  primary: Color(0xffFF5722),
+  secondary: Color(0xffFF7043),
+  tertiary: Color(0xff3E2723),
+  inversePrimary: Color(0xffFFCC80),
+));
---
+++ lib/theme/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Color(0xffFFF3E0),
+  primary: Color(0xffFF7043),
+  secondary: Color(0xffFFAB91),
+  tertiary: Color(0xffFFFFFF),
+  inversePrimary: Color(0xffD84315),
+));
